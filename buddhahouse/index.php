10bea3231b436157a1df74e4d4668ee7a62e0929
chasingdonis <cuixianning@hotmail.com>
2016-02-02 19:47:23 -0600

    php

---
+++ buddhahouse/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Buddha House</title>
+	<script src="http://use.edgefonts.net/josefin-slab.js"></script>
+	<script src="http://use.edgefonts.net/merriweather.js"></script>
+	<link rel="stylesheet" href="http://localhost:8888/wordpress/wp-content/themes/buddhahouse/style.css"/>
+</head>
+	<body>
+		<header>
+			<h1>Buddha House</h1>
+			<nav>
+				<ul>
+					<li>Home</li>
+					<li>WordPress</li>
+				</ul>
+			</nav>
+		</header>
+		<section>
+			<center><img src="http://localhost:8888/wordpress/wp-content/themes/buddhahouse/images/cat.jpg" /></center>
+			<section>
+				<p>I'm a serious cat and like to take selfies with serious poses.</p>
+				<p>I like to sleep, sometime run like a dog, jump up and down, and follow people to see what they do.</p>
+			</section>
+		</section>
+		<footer>
+			<h1>Contact Me</h1>
+			<p>Buddha House</p>
+			<p>2525 Orange Way, Orchard, CA 91104</p>
+			<p>Phone: (800) 424-3232</p>
+		</footer>
+	</body>
+</html>
---
+++ week1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>week1</title>
+</head> 
+<body>
+<?php
+	$names = array('Kelly Horigan', 'Anthony Peruzzo', 'Michelle Butts', 'Claire Larkins');
+	echo '<p>', $names[0], '</p>';
+	echo '<p>', $names[1], '</p>';
+	echo '<p>', $names[2], '</p>';
+	echo '<p>', $names[3], '</p>';
+?>
+</body>
+</html>
